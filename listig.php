1c0e9c610e73b56678eb582548fd3e7775bc4381
Andreas Ek <andreas@elseif.se>
2017-02-14 15:18:18 +0100

    Check for PHP>=7

--- listig.php
+++ listig.php
@@ -12,6 +12,20 @@ Text Domain: listig
 Domain Path: /languages
 */
 
+
+/**
+ * Check for PHP >= 7 and nag an error if not true!
+ */
+if(PHP_VERSION<7) {
+    add_action( 'admin_notices', 'listig_requires_php_version_7' );
+    function listig_requires_php_version_7() {
+        $class = 'notice notice-error';
+        $message = __( 'Plugin "listig" requires at least PHP7. Please upgrade PHP or inactivate the plugin!', 'listig' );
+        printf( '<div class="%1$s"><p>%2$s</p></div>', $class, $message );
+    }
+    return;
+}
+
 /**
  * Namespace autoload outside of Composer to reach WordPress plugin compability without Composer
  */
