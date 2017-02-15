cf59cf1e502b9abdcf7f9b726d80f5ab6fb21b37
Andreas Ek <andreas@elseif.se>
2017-02-15 14:36:38 +0100

    Loading scripts without laravel-mix versioning

--- globals/custom-filters.php
+++ globals/custom-filters.php
@@ -2,3 +2,7 @@
 /**
  * Custom filters within this plugin application for modifications
  */
+
+add_filter('listig/pluginData', function () {
+    return get_plugin_data(dirname(__DIR__) . '/listig.php', $markup = true, $translate = true);
+});
